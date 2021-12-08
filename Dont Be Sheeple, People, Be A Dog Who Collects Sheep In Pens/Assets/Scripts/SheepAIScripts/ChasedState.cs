bdbe0e69a4e4038d4fd49343d34caa431a1fb1df
solidsmyth <74252115+solidsmyth@users.noreply.github.com>
2021-12-08 15:13:32 -0500

    GD2-Final

--- Dont Be Sheeple, People, Be A Dog Who Collects Sheep In Pens/Assets/Scripts/SheepAIScripts/ChasedState.cs
+++ Dont Be Sheeple, People, Be A Dog Who Collects Sheep In Pens/Assets/Scripts/SheepAIScripts/ChasedState.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class ChasedState : State
 {
+    [SerializeField]
     public CaptureState captureState;
+    [SerializeField]
     public WanderState wanderState;
 
+    [SerializeField]
     public bool captured;
+    [SerializeField]
     public bool escaped;
 
     public override State RunCurrentState()
--- Dont Be Sheeple, People, Be A Dog Who Collects Sheep In Pens/Assets/Scripts/SheepAIScripts/WanderState.cs
+++ Dont Be Sheeple, People, Be A Dog Who Collects Sheep In Pens/Assets/Scripts/SheepAIScripts/WanderState.cs
@@ -1,22 +1,99 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class WanderState : State
 {
+    private NavMeshAgent navMeshAgent;
+    private Rigidbody rb;
+
+    [SerializeField]
     public ChasedState chaseState;
+    [SerializeField]
     public bool playerIsClose;
+
+    public float moveSpeed = 1f;
+    public float rotSpeed = 50f;
+
+    private bool isRotatingLeft = false;
+    private bool isRotatingRight = false;
+    private bool isWalking = false;
+
+    private void Awake()
+    {
+        navMeshAgent = gameObject.GetComponentInParent<NavMeshAgent>();
+    }
 
     public override State RunCurrentState()
     {
         if (playerIsClose)
         {
+            Debug.Log("Begin Chased");
             return chaseState;
         }
         else
         {
+            StartCoroutine("Wander");
+            if (isRotatingRight == true)
+            {
+                transform.Rotate(transform.up * Time.deltaTime * rotSpeed);
+            }
+            if (isRotatingLeft == true)
+            {
+                transform.Rotate(transform.up * Time.deltaTime * -rotSpeed);
+            }
+            if(isWalking == true)
+            {
+                transform.position += transform.forward * moveSpeed * Time.deltaTime;
+            }
+
+            navMeshAgent.destination = this.transform.position;
             return this;
         }
     }
 
+    IEnumerator Wander()
+    {
+        int rotTime = Random.Range(1, 3);
+        int rotateWait = Random.Range(1, 4);
+        int rotateLorR = Random.Range(1, 2);
+        int walkWait = Random.Range(1, 4);
+        int walkTime = Random.Range(1, 5);
+
+        yield return new WaitForSeconds(walkWait);
+        isWalking = true;
+        yield return new WaitForSeconds(walkTime);
+        isWalking = false;
+        yield return new WaitForSeconds(rotateWait);
+        if(rotateLorR == 1)
+        {
+            isRotatingRight = true;
+            yield return new WaitForSeconds(rotTime);
+            isRotatingRight = false;
+        }
+        if (rotateLorR == 2)
+        {
+            isRotatingLeft = true;
+            yield return new WaitForSeconds(rotTime);
+            isRotatingLeft = false;
+        }
+        yield return new WaitForSeconds(.5f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("Trigger");
+            playerIsClose = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerIsClose = false;
+        }
+    }
 }
